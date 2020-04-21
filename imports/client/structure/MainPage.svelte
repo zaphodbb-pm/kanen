cefab907a06e8d515fddd3629503d6b9bae337c4
PureColo <rainer@purecolo.ca>
2020-04-20 20:11:36 -0400

    Added routing.

---
+++ imports/client/structure/MainPage.svelte
@@ -0,0 +1,32 @@
+<script>
+
+    import Pages from 'svelte-router-spa/src/components/router.svelte'
+    import {routes} from './routes'
+
+</script>
+
+<div class="page-master-layout has-background-white">
+    <div class="columns">
+
+        <div class="column is-narrow is-hidden-touch is-hidden-desktop-only">
+            side nav
+        </div>
+
+        <div class="column">
+            <section id="page-layout">
+                <Pages {routes} />
+            </section>
+
+            <footer class="mt-5 has-border-top">footer</footer>
+        </div>
+
+    </div>
+</div>
+
+<style>
+
+
+
+</style>
+
+
