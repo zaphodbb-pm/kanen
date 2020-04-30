885820206ae7163da0f8b423ac5011173fe96213
PureColo <rainer@purecolo.ca>
2020-04-30 15:15:14 -0400

    Worked on routes.

--- imports/client/Navbar/Navbar_Brand.svelte
+++ imports/client/Navbar/Navbar_Brand.svelte
@@ -13,13 +13,13 @@
 
     import Icon from 'svelte-awesome/components/Icon.svelte';
     import { getContext } from 'svelte';
-    let logo = getContext("iconLogo");
+    let logo2 = getContext("iconLogo2");
 
 </script>
 
 
 <a href="/page-one" class="navbar-brand-insert d-flex pl-4 has-text-primary">
-    <Icon data={logo} scale="2.5" style="align-self: center;"/>
+    <Icon data={logo2} scale="3" style="align-self: center;"/>
 
     <img class="navbar-external-logo" src="/sweatcrew-logo.png" alt="logo">
 </a>
