10e251abf0726450a8f431fda4d586b2faf9ebe3
PureColo <rainer@purecolo.ca>
2020-06-13 15:14:30 -0400

    Worked on logged in components.

--- imports/both/Pages/Users/users_route.js
+++ imports/both/Pages/Users/users_route.js
@@ -39,6 +39,6 @@ export default {
         write: ["siteAdmin"]
     },
 
-    group: 2,                               // for side navigation; group routes into a block
+    group: 3,                               // for side navigation; group routes into a block
     isNavMobile: false,                     // (optional) show link in mobile nav block at bottom or top
 };
