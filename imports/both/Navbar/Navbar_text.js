5061442ac88935ec5afcc849fdc0af29e617a6db
PureColo <rainer@purecolo.ca>
2020-07-06 12:22:57 -0400

    Updated gdpr message.

--- imports/both/Navbar/Navbar_text.js
+++ imports/both/Navbar/Navbar_text.js
@@ -40,11 +40,9 @@ export const gdpr = {
 
         msg: "This website stores cookies on your computer. These cookies are used to collect information about " +
             "how you interact with our website and allow us to remember you. We use this information in order to improve and " +
-            "customize your browsing experience.  To find out more about the cookies we use, " +
-            "see our <a href='/privacy-policy/' target='_blank'>Privacy Policy.</a>. <br/><br/>" +
-            "If you decline, your information won’t be tracked when you visit this website. " +
-            "A single cookie will be used in your browser to remember your preference not to be tracked. " +
-            "Learn more about how to <a href='https://cookiesandyou.com/' target='_blank'>manage</a> your cookies ...",
+            "customize your browsing experience.  " +
+            "If you decline, your information won’t be tracked when you visit this website.  " +
+            "Learn more about how to <a href='https://cookiesandyou.com/' target='_blank'>manage</a> your cookies.",
 
         btnAccept: "Accept",
 
